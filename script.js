de8e1f3b67df99e1b72c6458b2576b0bf7f9a016
Christopher Variara <cvariara@gmail.com>
2023-09-14 17:43:42 -0400

    Added win conditions

--- script.js
+++ script.js
@@ -20,7 +20,9 @@ const gameBoard = () => {
     if (board[cell] === "X" || board[cell] === "O") {
       return false;
     } else {
-      board[cell] = player;
+      board[cell] = player.type;
+      player.moves.push(cell);
+      player.moves.sort();
       return true;
     }
   };
@@ -35,7 +37,6 @@ const gameBoard = () => {
 // controls the flow of the game and win/lose scenarios
 const GameController = () => {
   const board = gameBoard();
-  // board.displayBoard();
 
   const Player = (name, type) => {
     const moves = [];
@@ -49,88 +50,120 @@ const GameController = () => {
     };
   };
 
-  const player = Player("Chris", "X");
-  const ai = Player("Computer", "O");
-  // player.printString();
-  // ai.printString();
+  const player = Player("Player 1", "X");
+  const ai = Player("Player 2", "O");
 
   const players = [
     {
       name: player.name,
       type: player.type,
+      moves: player.moves,
     },
     {
       name: ai.name,
       type: ai.type,
+      moves: ai.moves,
     },
   ];
 
   let currentPlayer = players.find((player) => player.type === "X");
   const switchTurns = () => {
-    // console.log(`switching ${currentPlayer.name}`)
     currentPlayer = currentPlayer === players[0] ? players[1] : players[0];
   };
   const getCurrentPlayer = () => currentPlayer;
 
   const printNewRound = () => {
     board.displayBoard();
-    // console.log(`${getCurrentPlayer().name}'s turn`);
-  }
-  
+  };
+
   const playRound = (cell) => {
     // handles if cell was already chosen
-    if (board.fillBoardCell(cell, getCurrentPlayer().type)) {
+    if (board.fillBoardCell(cell, getCurrentPlayer())) {
+      /* winning logic */
+      if (checkWin(getCurrentPlayer())) {
+        console.log(`${getCurrentPlayer().name} wins!`)
+        return;
+      }
+
       switchTurns();
       printNewRound();
     } else {
       return;
     }
-    
-    /* winning logic */
   };
   printNewRound();
+
+  const checkWin = (player) => {
+    const nums = player.moves.map((str) => parseInt(str));
+    const winningCombos = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    // checks to see if the players movies are in the winningCombos
+    const foundCombo = winningCombos.find((combo) =>
+      combo.every((elem) => nums.includes(elem))
+    );
+
+    if (foundCombo) {
+      return true;
+    } else {
+      return false;
+    }
+  };
 
   // const boardIsFull = () => {
   //   return board.getBoard().every((cell) => cell !== "");
   // };
 
-
   return {
     playRound,
     getCurrentPlayer,
     getBoard: board.getBoard,
-    displayBoard: board.displayBoard
+    displayBoard: board.displayBoard,
+    checkWin
   };
 };
 
 const screenController = () => {
   const game = GameController();
-  const playerTurnDiv = document.querySelector('.turn');
-  const boardDiv = document.querySelector('.game');
-
+  const playerTurnDiv = document.querySelector(".turn");
+  const boardDiv = document.querySelector(".game");
+  
   const updateScreen = () => {
+    const currentPlayer = game.getCurrentPlayer();
     boardDiv.textContent = "";
 
     const board = game.getBoard();
-    const currentPlayer = game.getCurrentPlayer();
 
     playerTurnDiv.textContent = `${currentPlayer.name}'s turn...`;
 
     game.displayBoard();
-  }
+  };
 
   function handleClick(e) {
-    console.log(`FROM HANDLECLICK ${e.target.dataset.cell}`);
     const selectedCell = e.target.dataset.cell;
 
-    if (!selectedCell) return;
+    if (!selectedCell || game.checkWin(game.getCurrentPlayer())) return;
 
     game.playRound(selectedCell);
     updateScreen();
+
+    if (game.checkWin(game.getCurrentPlayer())) {
+      playerTurnDiv.textContent = `${game.getCurrentPlayer().name} Wins!`;
+      boardDiv.removeEventListener("click", handleClick);
+    }
   }
+
   boardDiv.addEventListener("click", handleClick);
 
   updateScreen();
-}
+};
 
 screenController();
